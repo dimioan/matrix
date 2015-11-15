22ac3f6690184a346916267099b1f625e0c3196a
Dimitris <bluesdio@googlemail.com>
2015-11-15 23:36:45 +0200

    new version

--- python_lab.py
+++ python_lab.py
@@ -1,45 +1,45 @@
-# version code ef5291f09f60+
-coursera = 1
+# version code 80e56511a793+
 # Please fill out this stencil and submit using the provided submission script.
 
 
 
 
 
-## 1: (Task 1) Minutes in a Week
+## 1: (Task 0.5.1) Minutes in a Week
 minutes_in_week = 60*24*7
 
 
 
-## 2: (Task 2) Remainder
+## 2: (Task 0.5.2) Remainder
 #For this task, your expression must use //
 remainder_without_mod = 2304811 - (2304811//47.)*47
 
 
 
-## 3: (Task 3) Divisibility
+## 3: (Task 0.5.3) Divisibility
 divisible_by_3 = (673 + 909)%3. == 0
 
 
 
-## 4: (Task 4) Conditional Expression
+## 4: (Task 0.5.4) Conditional Expression
+# Replace the ... with the expression 
 x = -9
 y = 1/2
 expression_val = 2**(y + 1/2) if x + 10 < 0 else 2**(y - 1/2)
 
 
 
-## 5: (Task 5) Squares Set Comprehension
+## 5: (Task 0.5.5) Squares Set Comprehension
 first_five_squares = { x**2 for x in {1,2,3,4,5} }
 
 
 
-## 6: (Task 6) Powers-of-2 Set Comprehension
+## 6: (Task 0.5.6) Powers-of-2 Set Comprehension
 first_five_pows_two = { 2**x for x in {0,1,2,3,4} }
 
 
 
-## 7: (Task 7) Double comprehension evaluating to nine-element set
+## 7: (Task 0.5.7) Double comprehension evaluating to nine-element set
 # Assign three-element sets to X1 and Y1 so that
 # {x*y for x in X1 for y in Y1} evaluates to a nine-element set.
 
@@ -48,16 +48,16 @@ Y1 = { 5, 6, 7 }
 
 
 
-## 8: (Task 8) Double comprehension evaluating to five-element set
+## 8: (Task 0.5.8) Double comprehension evaluating to five-element set
 # Assign disjoint three-element sets to X1 and Y1 so that
 # {x*y for x in X1 for y in Y1} evaluates to a five-element set.
 
-X2 = { ..., ..., ... }
-Y2 = { ..., ..., ... }
+X2 = { 0, 1, 2 }
+Y2 = { -1, -2, -4 }
 
 
 
-## 9: (Task 9) Set intersection as a comprehension
+## 9: (Task 0.5.9) Set intersection as a comprehension
 S = {1, 2, 3, 4}
 T = {3, 4, 5, 6}
 # Replace { ... } with a one-line set comprehension that evaluates to the intersection of S and T
@@ -65,22 +65,22 @@ S_intersect_T = {x for x in S if x in T}
 
 
 
-## 10: (Task 10) Average
+## 10: (Task 0.5.10) Average
 list_of_numbers = [20, 10, 15, 75]
 # Replace ... with a one-line expression that evaluates to the average of list_of_numbers.
 # Your expression should refer to the variable list_of_numbers, and should work
 # for a list of any length greater than zero.
-list_average = ... 
+list_average = (sum(list_of_numbers)/len(list_of_numbers)) if len(list_of_numbers) > 0 else float('nan')
 
 
 
-## 11: (Task 11) Cartesian-product comprehension
+## 11: (Task 0.5.11) Cartesian-product comprehension
 # Replace ... with a double list comprehension over ['A','B','C'] and [1,2,3]
 cartesian_product = ...
 
 
 
-## 12: (Task 12) Sum of numbers in list of list of numbers
+## 12: (Task 0.5.12) Sum of numbers in list of list of numbers
 LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
 # Replace ... with a one-line expression of the form sum([sum(...) ... ]) that
 # includes a comprehension and evaluates to the sum of all numbers in all the lists.
@@ -88,40 +88,40 @@ LofL_sum = ...
 
 
 
-## 13: (Task 13) Three-element tuples summing to zero
+## 13: (Task 0.5.14) Three-element tuples summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace [ ... ] with a one-line list comprehension in which S appears
 zero_sum_list = [ ... ] 
 
 
 
-## 14: (Task 14) Nontrivial three-element tuples summing to zero
+## 14: (Task 0.5.15) Nontrivial three-element tuples summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace [ ... ] with a one-line list comprehension in which S appears
 exclude_zero_list = [ ... ]
 
 
 
-## 15: (Task 15) One nontrivial three-element tuple summing to zero
+## 15: (Task 0.5.16) One nontrivial three-element tuple summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace ... with a one-line expression that uses a list comprehension in which S appears
 first_of_tuples_list = ...
 
 
 
-## 16: (Task 16) List and set differ
+## 16: (Task 0.5.17) List and set differ
 # Assign to example_L a list such that len(example_L) != len(list(set(example_L)))
 example_L = [...]
 
 
 
-## 17: (Task 17) Odd numbers
+## 17: (Task 0.5.18) Odd numbers
 # Replace {...} with a one-line set comprehension over a range of the form range(n)
 odd_num_list_range = {...}
 
 
 
-## 18: (Task 18) Using range and zip
+## 18: (Task 0.5.19) Using range and zip
 # In the line below, replace ... with an expression that does not include a comprehension.
 # Instead, it should use zip and range.
 # Note: zip() does not return a list. It returns an 'iterator of tuples'
@@ -129,7 +129,7 @@ range_and_zip = ...
 
 
 
-## 19: (Task 19) Using zip to find elementwise sums
+## 19: (Task 0.5.20) Using zip to find elementwise sums
 A = [10, 25, 40]
 B = [1, 15, 20]
 # Replace [...] with a one-line comprehension that uses zip together with the variables A and B.
@@ -139,7 +139,7 @@ list_sum_zip = [...]
 
 
 
-## 20: (Task 20) Extracting the value corresponding to key k from each dictionary in a list
+## 20: (Task 0.5.21) Extracting the value corresponding to key k from each dictionary in a list
 dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'Lewis'}, {'James':'Pierce', 'director':'Roger'}]
 k = 'James'
 # Replace [...] with a one-line comprehension that uses dlist and k
@@ -148,7 +148,7 @@ value_list = [...]
 
 
 
-## 21: (Task 21) Extracting the value corresponding to k when it exists
+## 21: (Task 0.5.22) Extracting the value corresponding to k when it exists
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
 #Replace [...] with a one-line comprehension 
@@ -158,20 +158,20 @@ value_list_modified_2 = [...] # <-- as you do here
 
 
 
-## 22: (Task 22) A dictionary mapping integers to their squares
+## 22: (Task 0.5.23) A dictionary mapping integers to their squares
 # Replace {...} with a one-line dictionary comprehension
 square_dict = {...}
 
 
 
-## 23: (Task 23) Making the identity function
+## 23: (Task 0.5.24) Making the identity function
 D = {'red','white','blue'}
 # Replace {...} with a one-line dictionary comprehension
 identity_dict = {...}
 
 
 
-## 24: (Task 24) Mapping integers to their representation over a given base
+## 24: (Task 0.5.25) Mapping integers to their representation over a given base
 base = 10
 digits = set(range(base))
 # Replace { ... } with a one-line dictionary comprehension
@@ -181,7 +181,7 @@ representation_dict = { ... }
 
 
 
-## 25: (Task 25) A dictionary mapping names to salaries
+## 25: (Task 0.5.26) A dictionary mapping names to salaries
 id2salary = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
 # Replace { ... } with a one-line dictionary comprehension that uses id2salary and names.
@@ -189,19 +189,19 @@ listdict2dict = { ... }
 
 
 
-## 26: (Task 26) Procedure nextInts
+## 26: (Task 0.5.28) Procedure nextInts
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
 def nextInts(L): return [ ... ]
 
 
 
-## 27: (Task 27) Procedure cubes
+## 27: (Task 0.5.29) Procedure cubes
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
 def cubes(L): return [ ... ] 
 
 
 
-## 28: (Task 28) Procedure dict2list
+## 28: (Task 0.5.30) Procedure dict2list
 # Input: a dictionary dct and a list keylist consisting of the keys of dct
 # Output: the list L such that L[i] is the value associated in dct with keylist[i]
 # Example: dict2list({'a':'A', 'b':'B', 'c':'C'},['b','c','a']) should equal ['B','C','A']
@@ -210,10 +210,16 @@ def dict2list(dct, keylist): return [ ... ]
 
 
 
-## 29: (Task 29) Procedure list2dict
+## 29: (Task 0.5.31) Procedure list2dict
 # Input: a list L and a list keylist of the same length
 # Output: the dictionary that maps keylist[i] to L[i] for i=0,1,...len(L)-1
 # Example: list2dict(['A','B','C'],['a','b','c']) should equal {'a':'A', 'b':'B', 'c':'C'}
 # Complete the procedure definition by replacing { ... } with a one-line dictionary comprehension
 def list2dict(L, keylist): return { ... }
+
+
+
+## 30: (Task 0.5.32) Generating all three-digit numbers over a given base
+# Complete the procedure definition by replacing { ... } with a one-line set comprehension
+def all_3_digit_numbers(base, digits): return { ... }
 
